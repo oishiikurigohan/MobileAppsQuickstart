84177a72a7316ea71770a1306c90ab3984de96a7
kurigohan <kurigohan_blog-contact@yahoo.co.jp>
2018-03-12 15:39:19 +0900

    Add project files.

---
+++ MobileAppsQuickstart/MobileAppsQuickstart/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MobileAppsQuickstart
+{
+    public static class Constants
+    {
+        // Replace strings with your Azure Mobile App endpoint.
+        public static string ApplicationURL = @"https://mobileappsquickstart.azurewebsites.net";
+    }
+}
